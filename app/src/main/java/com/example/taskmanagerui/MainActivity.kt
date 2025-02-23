36a222a24a0f23dd7738624ed674d66cab14423b
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-23 17:56:15 +0300

    Complete a task done UI

--- app/src/main/java/com/example/taskmanagerui/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerui/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanagerui.ui.theme.TaskManagerUITheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +30,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskManagerUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        BackgroundImage(
+                            painter = painterResource(R.drawable.ic_task_completed)
+                        )
+                        MainText(text = stringResource(R.string.main_text))
+                        SubText(text = stringResource(R.string.sub_text))
+                    }
                 }
             }
         }
@@ -31,9 +48,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun BackgroundImage(painter: Painter, modifier: Modifier = Modifier) {
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun MainText(text: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = text,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+            .padding(top = 24.dp, bottom = 8.dp),
+    )
+}
+
+@Composable
+fun SubText(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        fontSize = 16.sp,
         modifier = modifier
     )
 }
@@ -42,6 +79,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TaskManagerUITheme {
-        Greeting("Android")
+
     }
 }
